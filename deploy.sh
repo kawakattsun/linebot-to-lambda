09f8a75b48b028d0e6caf8049650e88a168fa729
kawakattsun <riverwin.akira>
2018-08-22 11:15:42 +0900

    buildファイル名は引数で指定する

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-GOOS=linux GOARCH=amd64 go build -o kawakatsu-line && \
-zip kawakatsu-line.zip kawakatsu-line client_secret.json && \
-aws lambda update-function-code --function-name kawakatsu-line-reply --zip-file fileb://./kawakatsu-line.zip --no-dry-run
+if [ $# -lt 1 ]; then
+  echo "Please specify an argument."
+  exit 1
+fi
+
+PROJECT_NANE=$1
+
+GOOS=linux GOARCH=amd64 go build -o ${PROJECT_NANE} && \
+zip ${PROJECT_NANE}.zip ${PROJECT_NANE} client_secret.json && \
+aws lambda update-function-code --function-name ${PROJECT_NANE}-reply --zip-file fileb://./${PROJECT_NANE}.zip --no-dry-run
